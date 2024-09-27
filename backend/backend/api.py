d336ab4122c7ba64b4e3e7555b3df5a6200ca22d
Lee Johnson <leejavaa@gmail.com>
2024-09-27 19:45:01 +0200

    Implement error handling in API

--- backend/backend/api.py
+++ backend/backend/api.py
@@ -1,4 +1,8 @@
+import logging
+
 from datetime import datetime
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from django.db import IntegrityError
 from typing import List
 
 from django.shortcuts import get_object_or_404
@@ -10,6 +14,7 @@ from ninja import NinjaAPI, Schema
 from ninja.responses import Response
 
 api = NinjaAPI()
+logger = logging.getLogger('gunicorn.error')
 
 class AnalysisOut(Schema):
     id: int
@@ -53,17 +58,36 @@ def serialize_analysis(analysis):
 
 @api.get("/analyses", response=List[AnalysisOut])
 def get_analyses(request):
-    qs = Analysis.objects.all()
-    return [serialize_analysis(analysis) for analysis in qs]
+    try:
+        logger.info("Fetching all analyses...")
+        qs = Analysis.objects.all()
+        serialized_analyses = [serialize_analysis(analysis) for analysis in qs]
+        logger.info(f"Successfully fetched {len(serialized_analyses)} analyses")
+
+        return serialized_analyses
+    except Exception as e:
+        logger.error(f"Error fetching analyses: {e}")
+        return Response({"error": "An error occurred while fetching analyses"}, status=500)
 
 @api.get("/analysis/{analysis_id}", response=AnalysisOut)
 def get_analysis(request, analysis_id: int):
-    analysis = get_object_or_404(Analysis, id=analysis_id)
-    return serialize_analysis(analysis)
+    try:
+        logger.info(f"Fetching analysis with id {analysis_id}...")
+        analysis = get_object_or_404(Analysis, id=analysis_id)
+        serialized_analysis = serialize_analysis(analysis)
+        logger.info(f"Successfully fetched analysis with id {analysis_id}")
+        return serialized_analysis
+    except ObjectDoesNotExist as e:
+        logger.error(f"Analysis with id {analysis_id} not found: {e}")
+        return Response({"error": f"Analysis with id {analysis_id} not found"}, status=404)
+    except Exception as e:
+        logger.error(f"Error fetching analysis with id {analysis_id}: {e}")
+        return Response({"error": f"Error fetching analysis with id {analysis_id}"}, status=500)
 
 @api.post("/create_analysis")
 def create_analysis(request, payload: AnalysisIn):
     try:
+        logger.info("Creating analysis...")
         map_obj = get_object_or_404(Map, name=payload.map)
         team_one_obj = get_object_or_404(Team, name=payload.team_one)
         team_two_obj = get_object_or_404(Team, name=payload.team_two)
@@ -77,13 +101,22 @@ def create_analysis(request, payload: AnalysisIn):
             output_file=payload.input_file,
         )
 
-        processing_result = process_video(analysis)
+        processing_result, processing_error_message = process_video(analysis)
 
         if processing_result:
+            logger.info("Analysis created successfully")
             return Response({"id": analysis.id}, status=201)
         else:
             analysis.delete()
-            return Response({"error": "Video processing failed"}, status=500)
+            logger.error("Analysis deleted, processing failed")
+            return Response({"error": processing_error_message}, status=500)
+    except IntegrityError as e:
+        logger.error(f"Error creating analysis: {e}")
+        return Response({"error": f"{e}: That's not good... Are you sure you entered everything correctly?"}, status=500)
+    except ValidationError as e:
+        logger.error(f"Error creating analysis: {e}")
+        return Response({"error": f"{e}: That's not good... Are you sure you entered everything correctly?"}, status=500)
     except Exception as e:
+        logger.error(f"Error creating analysis: {e}")
         return Response({"error": str(e)}, status=400)
 
