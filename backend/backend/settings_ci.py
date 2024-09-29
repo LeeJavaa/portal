d215dbc99d890acf6da692578f1c285d5f62acf0
Lee Johnson <leejavaa@gmail.com>
2024-09-29 12:52:54 +0200

    CI/CD fixed logging functionality to not make use of gunicorn

---
+++ backend/backend/settings_ci.py
@@ -0,0 +1,29 @@
+from .settings import *  # Import all settings from the main settings file
+
+# Override the LOGGING setting
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
+
+# Add any other CI-specific settings here
+DEBUG = False
