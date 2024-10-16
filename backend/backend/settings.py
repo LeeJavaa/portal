fc0a922c25618db9e735fe2063e44b8e958339c0
Lee Johnson <leejavaa@gmail.com>
2024-10-16 12:54:48 +0200

    Updated default settings to make use of environment variables. Included AWS settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -190,3 +193,6 @@ LOGGING = {
         },
     },
 }
+
+# AWS Stuff
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
