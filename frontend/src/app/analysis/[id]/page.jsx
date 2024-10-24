ea10cd88789c6eaac9e11f61f580a4bbf3327718
Lee Johnson <leejavaa@gmail.com>
2024-10-24 18:47:52 +0200

    Implement chart variety on map analysis frontend

--- frontend/src/app/analysis/[id]/page.jsx
+++ frontend/src/app/analysis/[id]/page.jsx
@@ -2,6 +2,14 @@
 import Scoreboard from "@/components/Scoreboard";
 import { Button } from "@/components/ui/button";
 import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card";
+import {
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
@@ -23,10 +31,58 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Trash2, Image as ImageIcon, Monitor } from "lucide-react";
-import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import playerMapPerformances from "@/mock/playerMapPerformance.json";
+import mapAnalyses from "@/mock/mapAnalysis.json";
+import {
+  Trash2,
+  Image as ImageIcon,
+  Crosshair,
+  Skull,
+  TrendingUp,
+} from "lucide-react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Cell,
+  Pie,
+  PieChart,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 export default function Page() {
+  const filteredData = playerMapPerformances.playerMapPerformances
+    .filter((performance) => performance.mapAnalysis === 1)
+    .map((performance) => ({
+      player: performance.player,
+      kills: performance.kills,
+      deaths: performance.deaths,
+      kdRatio: performance.kdRatio,
+    }));
+
+  const mapAnalysisData = mapAnalyses.mapAnalyses
+    .filter((mapAnalysis) => mapAnalysis.id === 1)
+    .map((mapAnalysis) => ({
+      teamOne: mapAnalysis.teamOne,
+      teamOneScore: mapAnalysis.teamOneScore,
+      teamTwo: mapAnalysis.teamTwo,
+      teamTwoScore: mapAnalysis.teamTwoScore,
+    }));
+
+  const pieChartData = [
+    {
+      name: mapAnalysisData[0].teamOne,
+      value: mapAnalysisData[0].teamOneScore,
+      fill: "var(--color-teamOne)",
+    },
+    {
+      name: mapAnalysisData[0].teamTwo,
+      value: mapAnalysisData[0].teamTwoScore,
+      fill: "var(--color-teamTwo)",
+    },
+  ];
+
   const playerData = [
     {
       name: "KENNY",
@@ -44,24 +100,26 @@ export default function Page() {
     },
   ];
 
-  const chartData = [
-    { month: "January", desktop: 186, mobile: 80 },
-    { month: "February", desktop: 305, mobile: 200 },
-    { month: "March", desktop: 237, mobile: 120 },
-    { month: "April", desktop: 73, mobile: 190 },
-    { month: "May", desktop: 209, mobile: 130 },
-    { month: "June", desktop: 214, mobile: 140 },
-  ];
-
   const chartConfig = {
-    desktop: {
-      label: "Desktop",
-      icon: Monitor,
+    kills: {
+      label: "Kills",
       color: "hsl(var(--chart-2))",
     },
-    mobile: {
-      label: "Mobile",
-      color: "hsl(var(--chart-3))",
+    deaths: {
+      label: "Deaths",
+      color: "hsl(var(--chart-4))",
+    },
+    kdRatio: {
+      label: "KD",
+      color: "hsl(var(--chart-5))",
+    },
+    teamOne: {
+      label: "Team One",
+      color: "hsl(var(--chart-2))",
+    },
+    teamTwo: {
+      label: "Team Two",
+      color: "hsl(var(--chart-1))",
     },
   };
 
@@ -113,23 +171,185 @@ export default function Page() {
       <div>Karachi, Hardpoint</div>
       <div>OpTic Texas vs NYSL GF, Call of Duty Championships 2024</div>
       <div>21 July 2024, 6pm</div>
-      <div className="w-[500px] mb-7">
-        <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
-          <BarChart accessibilityLayer data={chartData}>
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey="month"
-              tickLine={false}
-              tickMargin={10}
-              axisLine={false}
-              tickFormatter={(value) => value.slice(0, 3)}
-            />
-            <ChartTooltip content={<ChartTooltipContent />} />
-            <ChartLegend content={<ChartLegendContent />} />
-            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-            <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
-          </BarChart>
-        </ChartContainer>
+      <div className="flex justify-between">
+        <Card>
+          <CardHeader>
+            <CardTitle>Player Kills / Deaths</CardTitle>
+            <CardDescription>
+              The amount of kills vs deaths for each player
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ChartContainer
+              config={chartConfig}
+              className="min-h-[200px] w-full"
+            >
+              <BarChart accessibilityLayer data={filteredData}>
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey="player"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                  tickFormatter={(value) => value.slice(0, 3)}
+                />
+                <YAxis
+                  dataKey="kills"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                />
+                <ChartTooltip content={<ChartTooltipContent />} />
+                <ChartLegend content={<ChartLegendContent />} />
+                <Bar dataKey="kills" fill="var(--color-kills)" radius={4} />
+                <Bar dataKey="deaths" fill="var(--color-deaths)" radius={4} />
+              </BarChart>
+            </ChartContainer>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Player Kills</CardTitle>
+            <CardDescription>
+              The amount of kills vs deaths for each player
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ChartContainer
+              config={chartConfig}
+              className="min-h-[200px] w-full"
+            >
+              <BarChart accessibilityLayer data={filteredData}>
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey="player"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                  tickFormatter={(value) => value.slice(0, 3)}
+                />
+                <YAxis
+                  dataKey="kills"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                />
+                <ChartTooltip content={<ChartTooltipContent />} />
+                <ChartLegend content={<ChartLegendContent />} />
+                <Bar dataKey="kills" fill="var(--color-kills)" radius={4} />
+              </BarChart>
+            </ChartContainer>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Player Deaths</CardTitle>
+            <CardDescription>
+              The amount of kills vs deaths for each player
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ChartContainer
+              config={chartConfig}
+              className="min-h-[200px] w-full"
+            >
+              <BarChart accessibilityLayer data={filteredData}>
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey="player"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                  tickFormatter={(value) => value.slice(0, 3)}
+                />
+                <YAxis
+                  dataKey="deaths"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                />
+                <ChartTooltip content={<ChartTooltipContent />} />
+                <ChartLegend content={<ChartLegendContent />} />
+                <Bar dataKey="deaths" fill="var(--color-deaths)" radius={4} />
+              </BarChart>
+            </ChartContainer>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="flex justify-between">
+        <Card>
+          <CardHeader>
+            <CardTitle>Player K/D ratio</CardTitle>
+            <CardDescription>
+              The kills to death ratio for each player
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ChartContainer
+              config={chartConfig}
+              className="min-h-[200px] w-full"
+            >
+              <BarChart accessibilityLayer data={filteredData}>
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey="player"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                  tickFormatter={(value) => value.slice(0, 3)}
+                />
+                <YAxis
+                  dataKey="kdRatio"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                />
+                <ChartTooltip content={<ChartTooltipContent />} />
+                <ChartLegend content={<ChartLegendContent />} />
+                <Bar dataKey="kdRatio">
+                  {filteredData.map((item) => (
+                    <Cell
+                      key={item.player}
+                      fill={
+                        item.kdRatio > 1
+                          ? "hsl(var(--chart-1))"
+                          : "hsl(var(--chart-2))"
+                      }
+                    />
+                  ))}
+                </Bar>
+              </BarChart>
+            </ChartContainer>
+          </CardContent>
+        </Card>
+        <Card className="flex flex-col">
+          <CardHeader className="items-center pb-0">
+            <CardTitle>Domination analysis</CardTitle>
+            <CardDescription>See how close the game was</CardDescription>
+          </CardHeader>
+          <CardContent className="flex-1 pb-0">
+            <ChartContainer
+              config={chartConfig}
+              className="mx-auto aspect-square max-h-[250px]"
+            >
+              <PieChart>
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent hideLabel />}
+                />
+                <Pie data={pieChartData} dataKey="value" nameKey="name" />
+              </PieChart>
+            </ChartContainer>
+          </CardContent>
+          <CardFooter className="flex-col gap-2 text-sm">
+            <div className="flex items-center gap-2 font-medium leading-none">
+              Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+            </div>
+            <div className="leading-none text-muted-foreground">
+              Showing total visitors for the last 6 months
+            </div>
+          </CardFooter>
+        </Card>
       </div>
     </main>
   );
