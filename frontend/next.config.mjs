9ebb1d46e8071a35b253c7dfbb9a14365d7bb810
Lee Johnson <leejavaa@gmail.com>
2024-11-27 16:37:48 +0200

    Update next config to allow for viewing thumbnails from S3

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['localhost'],
-    },
+  images: {
+    domains: ["portal-web-public.s3.us-east-1.amazonaws.com"],
+  },
 };
 
 export default nextConfig;
