c33655507d349920ef52f016e2b76cd19557df35
Lee Johnson <leejavaa@gmail.com>
2024-11-28 14:20:06 +0200

    Update general data for frontend

--- frontend/src/data/general.js
+++ frontend/src/data/general.js
@@ -45,25 +45,64 @@ export const TEAMS = {
     name: "OpTic Texas",
     identifiers: ["optic texas", "optic", "optictexas"],
   },
-  NYSL: {
-    code: "nysl",
-    name: "New York Subliners",
-    identifiers: [
-      "new york subliners",
-      "nysl",
-      "subliners",
-      "newyorksubliners",
-    ],
-  },
   FAZE: {
     code: "af",
     name: "Atlanta FaZe",
     identifiers: ["atlanta faze", "faze", "atlantafaze"],
   },
+  BREACH: {
+    code: "bb",
+    name: "Boston Breach",
+    identifiers: ["boston breach", "breach", "bostonbreach"],
+  },
+  ROYAL_RAVENS: {
+    code: "crr",
+    name: "Carolina Royal Ravens",
+    identifiers: [
+      "carolina royal ravens",
+      "royal ravens",
+      "carolinaroyalravens",
+    ],
+  },
+  CLOUD9: {
+    code: "c9",
+    name: "Cloud9 New York",
+    identifiers: ["cloud9 new york", "cloud9", "cloud9ny", "c9"],
+  },
+  GUERRILLAS: {
+    code: "lag",
+    name: "Los Angeles Guerrillas",
+    identifiers: ["los angeles guerrillas", "guerrillas", "lag"],
+  },
+  THIEVES: {
+    code: "lat",
+    name: "Los Angeles Thieves",
+    identifiers: ["los angeles thieves", "thieves", "lat"],
+  },
+  HERETICS: {
+    code: "mh",
+    name: "Miami Heretics",
+    identifiers: ["miami heretics", "heretics", "miamiheretics"],
+  },
+  ROKKR: {
+    code: "mr",
+    name: "Minnesota Rokkr",
+    identifiers: ["minnesota rokkr", "rokkr", "minnesotarokkr"],
+  },
+  SURGE: {
+    code: "vs",
+    name: "Vancouver Surge",
+    identifiers: ["vancouver surge", "surge", "vancouversurge"],
+  },
   ULTRA: {
     code: "tu",
     name: "Toronto Ultra",
     identifiers: ["toronto ultra", "ultra", "torontoultra"],
+  },
+  FALCONS: {
+    code: "vf",
+    name: "Vegas Falcons",
+    identifiers: ["vegas falcons", "falcons", "vegasfalcons"],
   },
 };
 
@@ -112,7 +151,7 @@ export const PLAYERS = {
     clean: "Sib",
   },
   KISMET: {
-    dirty: "Kismet",
+    dirty: "KiSMET",
     clean: "Kismet",
   },
   SKYZ: {
@@ -120,8 +159,172 @@ export const PLAYERS = {
     clean: "Skyz",
   },
   HYDRA: {
-    dirty: "Hydra",
-    clean: "Hydra",
+    dirty: "HyDra",
+    clean: "HyDra",
+  },
+  ATTACH: {
+    dirty: "Attach",
+    clean: "Attach",
+  },
+  KREMP: {
+    dirty: "Kremp",
+    clean: "Kremp",
+  },
+  MACK: {
+    dirty: "Mack",
+    clean: "Mack",
+  },
+  ABEZY: {
+    dirty: "AbeZy",
+    clean: "Abezy",
+  },
+  CELLIUM: {
+    dirty: "Cellium",
+    clean: "Cellium",
+  },
+  DRAZAH: {
+    dirty: "Drazah",
+    clean: "Drazah",
+  },
+  SIMP: {
+    dirty: "Simp",
+    clean: "Simp",
+  },
+  CAMMY: {
+    dirty: "Cammy",
+    clean: "Cammy",
+  },
+  OWAKENING: {
+    dirty: "Owakening",
+    clean: "Owakening",
+  },
+  PURJ: {
+    dirty: "Purj",
+    clean: "Purj",
+  },
+  SNOOPY: {
+    dirty: "Snoopy",
+    clean: "Snoopy",
+  },
+  GWINN: {
+    dirty: "Gwinn",
+    clean: "Gwinn",
+  },
+  SLASHER: {
+    dirty: "Slasher",
+    clean: "Slasher",
+  },
+  TJHALY: {
+    dirty: "TJHaly",
+    clean: "TJHaly",
+  },
+  VIVID: {
+    dirty: "Vivid",
+    clean: "Vivid",
+  },
+  BEANS: {
+    dirty: "Beans",
+    clean: "Beans",
+  },
+  CLEANX: {
+    dirty: "CleanX",
+    clean: "CleanX",
+  },
+  INSIGHT: {
+    dirty: "Insight",
+    clean: "Insight",
+  },
+  JOEDECEIVES: {
+    dirty: "JoeDeceives",
+    clean: "JoeDeceives",
+  },
+  EXNID: {
+    dirty: "Exnid",
+    clean: "Exnid",
+  },
+  KHHX: {
+    dirty: "Khhx",
+    clean: "Khhx",
+  },
+  KINGABODY: {
+    dirty: "Kingabody",
+    clean: "Kingabody",
+  },
+  ROXAS: {
+    dirty: "Roxas",
+    clean: "Roxas",
+  },
+  LYNNZ: {
+    dirty: "Lyynnz",
+    clean: "Lyynnz",
+  },
+  PRIESTAHH: {
+    dirty: "Priestahh",
+    clean: "Priestahh",
+  },
+  ENVOY: {
+    dirty: "Envoy",
+    clean: "Envoy",
+  },
+  GHOSTY: {
+    dirty: "Ghosty",
+    clean: "Ghosty",
+  },
+  SCRAP: {
+    dirty: "Scrap",
+    clean: "Scrap",
+  },
+  LUCKY: {
+    dirty: "Lucky",
+    clean: "Lucky",
+  },
+  METTALZ: {
+    dirty: "MettalZ",
+    clean: "Mettalz",
+  },
+  REEAL: {
+    dirty: "ReeaL",
+    clean: "Reeal",
+  },
+  RENKOR: {
+    dirty: "RenKoR",
+    clean: "Renkor",
+  },
+  VIKUL: {
+    dirty: "Vikul",
+    clean: "Vikul",
+  },
+  ESTREAL: {
+    dirty: "Estreal",
+    clean: "Estreal",
+  },
+  GIO: {
+    dirty: "Gio",
+    clean: "Gio",
+  },
+  NERO: {
+    dirty: "Nero",
+    clean: "Nero",
+  },
+  PAULEHX: {
+    dirty: "PaulEhx",
+    clean: "PaulEhx",
+  },
+  O4: {
+    dirty: "04",
+    clean: "04",
+  },
+  ABUZAH: {
+    dirty: "Abuzah",
+    clean: "Abuzah",
+  },
+  HICKSY: {
+    dirty: "Hicksy",
+    clean: "Hicksy",
+  },
+  NASTIE: {
+    dirty: "Nastie",
+    clean: "Nastie",
   },
 };
 
